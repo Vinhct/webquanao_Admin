8a82c4b131d4feee6cfb4098700ae6f84dbed1a8
NamDuong <namduong220203@gmail.com>
2024-10-30 15:43:21 +0700

    fix code in Multiselect and orderId/route

--- app/api/orders/[orderId]/route.ts
+++ app/api/orders/[orderId]/route.ts
@@ -4,7 +4,7 @@ import Product from "@/lib/models/Product";
 import { connectToDB } from "@/lib/mongoDB";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest, { params }: { params: { orderId: String }}) => {
+export const GET = async (req: NextRequest, { params }: { params: { orderId: string }}) => {
   try {
     await connectToDB()
 
